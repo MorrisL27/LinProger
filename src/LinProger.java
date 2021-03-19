c5297831d4ea9760d62cc7f3ea71c2f04f10b415
MorrisL27 <18252214@life.hkbu.edu.hk>
2021-03-19 23:14:21 +0800

    Change variable name to rowZ, rowS respectively

--- src/LinProger.java
+++ src/LinProger.java
@@ -1,33 +1,14 @@
 public class LinProger {
-
-//    double[] f = {-2, 3, -6, 0, 0, 0, 0, 0, 0};
-//    double[] fM = {5, -3, -4, -1, -1, 0, 0, 0, 13};
-    //double[][] A = new double[3][9];
-
-
-//    double[] f = {-4, -1, 0, 0, 0, 0, 0};
-//    double[] fM = {7, 4, -1, 0, 0, 0, 9};
-    //double[][] A = new double[3][7];
-
-    //A3.3
-//    double[] f = {3, 4, 0, 0, 0, 0};
-//    double[] fM = {0, 0, 0, 0, 0, 0};
-//    double[][] A = new double[3][6];
-
-    //Tutorial 8.1
-//    double[] f = {3, 2, 0, 0, 0, 0};
-//    double[] fM = {0, 0, 0, 0, 0, 0};
-//    double[][] A = new double[3][6];
-
-
-    double[] f;
+    double[] rowZ;
     double[] fM;
-    double[][] A; // use lower case
+    double[][] rowS;
     double[] b;
     double[][] Aeq; // use lower case
     double[][] beq;
     double[] lb;
     double[] ub;
+
+    double[][] tableau;
 
     int numVar;
     int numCons;
@@ -44,12 +25,24 @@ public class LinProger {
         return numCons;
     }
 
-    public void setF(double[] f) {
-        this.f = f;
+    public void setZ(double[] rowZ) {
+        this.rowZ = rowZ;
+    }
+
+    public void setS(double[][] rowS) {
+        this.rowS = rowS;
+    }
+
+    public void set(double[] rowA, int index) {
+        //this.rowS[index] = rowA;
+    }
+
+    public void setF(double[] rowZ) {
+        //this.rowZ = rowZ;
     }
 
     public void setA(double[] rowA, int index) {
-        this.A[index] = rowA;
+        //this.rowS[index] = rowA;
     }
 
     public void setFM(double[] fM) {
@@ -60,23 +53,22 @@ public class LinProger {
         this.b = b;
     }
 
+    public void setTableau() {
+        tableau = new double[numCons][numVar + numCons + 1];
+    }
 
     public LinProger() {
         numVar = 1;
         numCons = 1;
         setDimension(numVar, numCons);
 
-        /*
-        //A3.1
-        f = new double[] {2, 2, 0, 0, 0, 0, 0, 0, 0};
+        rowZ = new double[] {2, 2, 0, 0, 0, 0, 0, 0, 0};
         fM = new double[] {0, 0, 0, 0, 0, 0, 0, 0, 0};
-        A = new double[4][9];
-
-        A[0] = new double[] {-1, -1, 0, 0, 1, 0, 0, 0, 1};
-        A[1] = new double[] {0, 1, -1, 1, 0, 1, 0, 0, 2};
-        A[2] = new double[] {5, 0, 2, -2, 0, 0, 1, 0, 0};
-        A[3] = new double[] {-1, 0, 1, 1, 0, 0, 0, 1, 0};
-        */
+        rowS = new double[4][9];
+        rowS[0] = new double[] {-1, -1, 0, 0, 1, 0, 0, 0, 1};
+        rowS[1] = new double[] {0, 1, -1, 1, 0, 1, 0, 0, 2};
+        rowS[2] = new double[] {5, 0, 2, -2, 0, 0, 1, 0, 0};
+        rowS[3] = new double[] {-1, 0, 1, 1, 0, 0, 0, 1, 0};
     }
 
     // n: number of variables
@@ -84,16 +76,16 @@ public class LinProger {
     public void setDimension(int n, int m) {
         numVar = n;
         numCons = m;
-        f = new double[numVar + numCons + 1];
+        rowZ = new double[numVar + numCons + 1];
         fM = new double[numVar + numCons + 1];
-        A = new double[numCons][numVar + numCons + 1];
+        rowS = new double[numCons][numVar + numCons + 1];
     }
 
     void showTableau() {
         // check z
         System.out.print("f:    ");
-        for (int i = 0; i < f.length; i++) {
-            System.out.printf(" %+.2f",f[i]);
+        for (int i = 0; i < rowZ.length; i++) {
+            System.out.printf(" %+.2f", rowZ[i]);
         }
         System.out.println();
 
@@ -104,37 +96,15 @@ public class LinProger {
         System.out.println();
 
         // check A
-        for (int j = 0; j < A.length; j++) {
-            System.out.print("A[" + j + "]: ");
-            for (int i = 0; i < A[0].length; i++) {
-                System.out.printf(" %+.2f", A[j][i]);
+        for (int j = 0; j < rowS.length; j++) {
+            System.out.print("S[" + j + "]: ");
+            for (int i = 0; i < rowS[0].length; i++) {
+                System.out.printf(" %+.2f", rowS[j][i]);
             }
             System.out.println();
         }
         System.out.println();
     }
-
-//        A[0] = new double[] {3, -4, -6, -1, 0, 1, 0, 0, 2};
-//        A[1] = new double[] {2, 1, 2, 0 , -1, 0, 1, 0, 11};
-//        A[2] = new double[] {1, 3, -2, 0, 0, 0, 0, 1, 5};
-
-
-//
-//        A[0] = new double[] {3, 1, 0, 1, 0, 0, 3};
-//        A[1] = new double[] {4, 3, -1, 0 , 1, 0, 6};
-//        A[2] = new double[] {1, 2, 0, 0, 0, 1, 4};
-
-
-
-    //A3.3
-//        A[0] = new double[] {1, 2, 1, 0, 0, 10};
-//        A[1] = new double[] {1, 1, 0, 1, 0, 8};
-//        A[2] = new double[] {3, 5, 0, 0, 1, 26};
-
-    // Tutorial 8.1
-//        A[0] = new double[] {1, 1, 1, 0, 0, 3};
-//        A[1] = new double[] {2, 1, 0, 1, 0, 4};
-//        A[2] = new double[] {4, 3, 0, 0, 1, 10};
 
     void linProg() {
         showTableau();
@@ -161,12 +131,12 @@ public class LinProger {
                 entering = -1;
 
 
-                for (int i = 0; i < f.length - 1; i++) {
+                for (int i = 0; i < rowZ.length - 1; i++) {
                     if (fM[i] == 0.0) {
-                        if (f[i] > 0) {
+                        if (rowZ[i] > 0) {
                             allNegative = false;
-                            if (max <= f[i]) {
-                                max = f[i];
+                            if (max <= rowZ[i]) {
+                                max = rowZ[i];
                                 entering = i;
                             }
                         }
@@ -181,10 +151,10 @@ public class LinProger {
 
             int leaving = -1;
             double min = 100000000000000000.0;
-            for (int i = 0; i < A.length; i++) {
-                if (A[i][entering] > 0) {
-                    double result = A[i][A[i].length - 1] / A[i][entering];
-                    System.out.println("result = " + A[i][A[i].length - 1] + "/" + A[i][entering] + " = " + result);
+            for (int i = 0; i < rowS.length; i++) {
+                if (rowS[i][entering] > 0) {
+                    double result = rowS[i][rowS[i].length - 1] / rowS[i][entering];
+                    System.out.println("result = " + rowS[i][rowS[i].length - 1] + "/" + rowS[i][entering] + " = " + result);
                     if (result < min) {
                         min = result;
                         leaving = i;
@@ -198,35 +168,35 @@ public class LinProger {
             }
 
 
-            double factor = A[leaving][entering];
+            double factor = rowS[leaving][entering];
 
-            for (int i = 0; i < A[leaving].length; i++) {
-                A[leaving][i] = A[leaving][i] / factor;
+            for (int i = 0; i < rowS[leaving].length; i++) {
+                rowS[leaving][i] = rowS[leaving][i] / factor;
             }
 
             factor = fM[entering];
             for (int i = 0; i < fM.length; i++) {
-                fM[i] = fM[i] - factor * A[leaving][i];
+                fM[i] = fM[i] - factor * rowS[leaving][i];
                 if (Math.abs(fM[i]) < 0.0001) {
                     fM[i] = 0;
                 }
             }
 
-            factor = f[entering];
-            for (int i = 0; i < f.length; i++) {
-                f[i] = f[i] - factor * A[leaving][i];
+            factor = rowZ[entering];
+            for (int i = 0; i < rowZ.length; i++) {
+                rowZ[i] = rowZ[i] - factor * rowS[leaving][i];
             }
 
 
 
-            for (int i = 0; i < A.length; i++) {
+            for (int i = 0; i < rowS.length; i++) {
                 if (i == leaving) {
                     continue;
                 }
 
-                factor = A[i][entering];
-                for (int j = 0; j < A[i].length; j++) {
-                    A[i][j] = A[i][j] - factor * A[leaving][j];
+                factor = rowS[i][entering];
+                for (int j = 0; j < rowS[i].length; j++) {
+                    rowS[i][j] = rowS[i][j] - factor * rowS[leaving][j];
                 }
             }
 
